eb953153748245cdbc09b47d7e8b2a1d964bb913
ARNAB-BOTMAS <arnabmondal203@gmail.com>
2023-12-15 19:30:02 +0530

    first commit

--- script/animation.js
+++ script/animation.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     const observer = new IntersectionObserver((entries) => {
         entries.forEach((entry) => {
-            console.log(entry);
+            // console.log(entry);
             if (entry.isIntersecting) {
                 entry.target.classList.add('show');
             } else {
@@ -11,5 +11,54 @@ document.addEventListener('DOMContentLoaded', () => {
     });
     
     const hiddenElements = document.querySelectorAll('.hidden');
+    hiddenElements.forEach((el) => observer.observe(el));
+});
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            // console.log(entry);
+            if (entry.isIntersecting) {
+                entry.target.classList.add('show');
+            } else {
+                entry.target.classList.remove('show');
+            }
+        });
+    });
+    
+    const hiddenElements = document.querySelectorAll('.hidden-left');
+    hiddenElements.forEach((el) => observer.observe(el));
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            // console.log(entry);
+            if (entry.isIntersecting) {
+                entry.target.classList.add('show');
+            } else {
+                entry.target.classList.remove('show');
+            }
+        });
+    });
+    
+    const hiddenElements = document.querySelectorAll('.hidden-right');
+    hiddenElements.forEach((el) => observer.observe(el));
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            // console.log(entry);
+            if (entry.isIntersecting) {
+                entry.target.classList.add('show');
+            } else {
+                entry.target.classList.remove('show');
+            }
+        });
+    });
+    
+    const hiddenElements = document.querySelectorAll('.hidden-in-out');
     hiddenElements.forEach((el) => observer.observe(el));
 });
--- script/script.js
+++ script/script.js
@@ -64,3 +64,79 @@ function download(){
     // Remove the anchor element from the document
     document.body.removeChild(link);
 }
+
+function changeBorder(formID){
+    var id = document.getElementById(formID);
+    id.style.border = "3px solid #81B214";
+}
+
+function resetBorder(formID) {
+    var id = document.getElementById(formID);
+    id.style.border = "3px solid #232d38"; // Change this to your desired default color
+}
+
+function sendEmail(){
+    var name = document.getElementById("name");
+    var phone = document.getElementById("phone");
+    var email = document.getElementById("email");
+    var message = document.getElementById("message");
+    var errorMsg = document.getElementById('error-message');
+    if(name.value === ""){
+        name.style.border = "3px solid red";
+    } else if(phone.value === ""){
+        phone.style.border = "3px solid red";
+    } else if(email.value === ""){
+        email.style.border = "3px solid red";
+    } else if(message.value === ""){
+        message.style.border = "3px solid red";
+    } else {
+        var body = `
+            <div style="font-family: Arial, sans-serif; background-color: #f4f4f4; padding: 20px; border-radius: 5px;">
+                <h2>Contact Information:</h2>
+                <ul style="list-style: none; padding: 0;">
+                    <li><strong>Name:</strong> ${name.value}</li>
+                    <li><strong>Phone:</strong> ${phone.value}</li>
+                    <li><strong>Email:</strong> ${email.value}</li>
+                </ul>
+                <div style="padding: 10px; border-left: 2px solid #555;">
+                    <p><strong>Message:</strong></p>
+                    <p style="margin-left: 10px;">${message.value}</p>
+                </div>
+            </div>
+        `;
+
+        Email.send({
+            Host: "smtp.elasticemail.com",
+            Username: "amansmas8016@gmail.com",
+            Password: "14FBF103F1DFFBA20B7C636EC073C13977D2",
+            To: 'arnabmondal203@gmail.com',
+            From: "amansmas8016@gmail.com",
+            Subject: "This is the subject",
+            Body: body
+        })
+            .then(function (msg) {
+                // Success: Email sent
+                // alert(message);
+                errorMsg.textContent = "Thank you, I will contact you later";
+                errorMsg.style.color = "#9ADE7B";
+                // Clear input fields
+                name.value = "";
+                phone.value = "";
+                email.value = "";
+                message.value = "";
+                setTimeout(function (){
+                    errorMsg.textContent = "";
+                }, 3000);
+            })
+            .catch(function (error) {
+                // Handle error
+                errorMsg.textContent = "Server Problem, Try Again";
+                errorMsg.style.color = "red";
+                // console.error("Error sending email:", error);
+                setTimeout(function (){
+                    errorMsg.textContent = "";
+                }, 3000);
+            });
+        
+    }
+}
