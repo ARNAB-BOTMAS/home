8ba5bbb812606c13e136e1c314e84a9ce08cf330
Arnab Mondal <92840854+ARNAB-BOTMAS@users.noreply.github.com>
2024-01-02 18:26:34 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 // config.js
 const config = {
-    API_KEY: 'o.NiP9A3dx7xCIJJbgoXxuvH08bGyX5skO', // Replace with your actual API key
+    // API_KEY: 'o.NiP9A3dx7xCIJJbgoXxuvH08bGyX5skO',
+    API_KEY: 'o.Me681Ne8kUljwbxl7DBEC0vbI3T51NYX'// Replace with your actual API key
   };
